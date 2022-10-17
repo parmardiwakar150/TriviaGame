d6298ad400ce569eeb016a3bc4f32393f2a1f929
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2022-10-17 23:05:55 +0530

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Diwakar Parmar on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
